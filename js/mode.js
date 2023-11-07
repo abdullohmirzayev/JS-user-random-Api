cc548576b6896054adb568e7b9a29b92be5e7336
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-11-08 00:28:06 +0500

    added mode fuction at localStorage safe icon dark-mode and toggleModel main function

--- js/mode.js
+++ js/mode.js
@@ -1,3 +1,28 @@
-const body = document.querySelector('body')
-const darkBtn = document.getElementById('dark-btn')
-const lightBtn = document.getElementById('light-btn')
+const body = document.querySelector("body");
+const darkBtn = document.getElementById("dark-btn");
+const lightBtn = document.getElementById("light-btn");
+
+// togel mode
+const modeLocal = localStorage.getItem("mode");
+
+if (modeLocal) {
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+  body.classList.add("dark-mode");
+}
+
+const toggelModeBtn = () => {
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+  body.classList.toggle("dark-mode");
+};
+
+darkBtn.addEventListener("click", () => {
+  toggelModeBtn();
+  localStorage.setItem("mode", "dark-mode");
+});
+
+lightBtn.addEventListener("click", () => {
+  toggelModeBtn();
+  localStorage.setItem("mode", "");
+});
